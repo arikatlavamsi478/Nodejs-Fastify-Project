ea9fc92c844bf364291bb3e02a77bcbc0be580ad
CodeSandbox <support@codesandbox.io>
2023-07-11 16:03:46 +0000

    "Initial commit"

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+apollo-server-fastify needs fastify@2 => check package.json
+*/
+const fastify = require("fastify");
+const { ApolloServer, gql } = require("apollo-server-fastify");
+const fs = require("fs");
+
+const port = 4000;
+
+const app = fastify();
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql(fs.readFileSync("./schema.graphql", { encoding: "utf8" }));
+
+// Provide resolver functions for your schema fields
+const resolvers = require("./resolvers");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my nodejs-fastify with graphql api");
+});
+app.register(server.createHandler());
+app.listen(port, () => console.info(`Server started on port ${port}`));
